9d23d691c1b901f9129c5dff06e8ffa3dbcd9ab2
buunguyen97 <buulenguyen@gmail.com>
2021-02-23 13:33:24 +0700

    moi

---
+++ class/goc.php
@@ -0,0 +1,11 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT']."\\news\\config.php";
+class goc{
+    protected  $db;
+    function  __construct()
+    {
+        $this->db = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
+        $this->db -> set_charset("utf8");
+
+    }
+}
